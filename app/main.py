e0dc4632d82d03fcae5e60c11b9383cc8fb85f3a
lucy lima <lucyatmosphere@gmail.com>
2026-01-07 19:42:38 -0300

    step 8 pass

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-from ast import arg
 import sys
 import os
-import pathlib
+import subprocess
 
 def main():
     builtin_commands = ['cd', 'echo', 'type', 'exit']
@@ -43,10 +42,35 @@ def main():
                         continue
                 else:
                     sys.stdout.write(f"{args[0]} not found\n")
-            
-            case _:
-                sys.stdout.write(f'{command}: command not found\n')
 
+            case _:
+                result = find_exec(command, args)
+                if result == 1:
+                    sys.stdout.write(f'{command}: command not found\n')
+                elif result == 2:
+                    sys.stdout.write("System path not found\n")
+
+
+def find_exec(command, args):
+    env = os.getenv("PATH", "Path not found")
+
+    if env == "Path not found":
+        return 2
+
+    env = env.split(':')
+
+    for folder in env:
+        exec = os.path.join(folder, command)
+
+        full_command = [command, *args]
+
+        if os.access(exec, os.X_OK):
+            subprocess.run(full_command)
+            return 0
+        else:
+            continue
+    return 1
+ 
 
 if __name__ == "__main__":
     main()
