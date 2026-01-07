e495be27f49793166eb9d30c599dd08a0c309dfe
lucy lima <lucyatmosphere@gmail.com>
2026-01-07 15:13:43 -0300

    step 3 pass

--- app/main.py
+++ app/main.py
@@ -2,11 +2,16 @@ import sys
 
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    command = input()
-    if command != "cd" or command != "echo":
-        sys.stdout.write(f'{command}: command not found')
+    while True:
+        sys.stdout.write("$ ")
+        command = input()
+        match command:
+            case "cd":
+                pass
+            case "echo":
+                pass
+            case _:
+                sys.stdout.write(f'{command}: command not found\n')
 
 
 if __name__ == "__main__":
