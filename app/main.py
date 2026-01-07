c4a20428c5fa249d17eab61db91760d8fba369c5
lucy lima <lucyatmosphere@gmail.com>
2026-01-07 16:32:03 -0300

    stage 6 pass

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtin_commands = ['cd', 'echo', 'type', 'exit']
     while True:
         sys.stdout.write("$ ")
         user_input = input()
@@ -14,12 +15,20 @@ def main():
         match command:
             case "cd":
                 pass
+                
             case "echo":
                 result = " ".join(args)
                 sys.stdout.write(f"{result}\n")
-                
+
             case "exit":
                 sys.exit(0)
+
+            case "type":
+                if args[0] in builtin_commands:
+                    sys.stdout.write(f"{args[0]} is a shell builtin\n")
+                else:
+                    sys.stdout.write(f"{args[0]} not found\n")
+            
             case _:
                 sys.stdout.write(f'{command}: command not found\n')
 
